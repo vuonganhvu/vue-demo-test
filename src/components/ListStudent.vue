442722588cd2e949b810d18aabe45787ef2e3555
anhvv <anh.vu1@ntq-solution.com.vn>
2018-09-21 10:23:15 +0700

    integrate axios vue

--- src/components/ListStudent.vue
+++ src/components/ListStudent.vue
@@ -11,7 +11,7 @@
        </tr>
        </thead>
        <tbody>
-       <tr v-for="(student, index) in students" :key="index">
+       <tr v-for="(student, index) in students" :key="index" @click="chooseStudent(student.id)">
          <td>{{index}}</td>
          <td>{{student.id}}</td>
          <td>{{student.name}}</td>
@@ -19,11 +19,52 @@
        </tr>
        </tbody>
      </table>
+     <div v-if="show" id="submit-review">
+       <h2>Submit a Review</h2>
+       <hr/>
+
+       <form>
+         <div class="form-group">
+           <label for="email">Email:</label>
+           <input type="email" class="form-control" id="email"
+                  v-model="review.reviewerEmail"
+                  placeholder="Enter your email address">
+         </div>
+
+         <div class="form-group">
+           <label for="name">Name:</label>
+           <input type="text" class="form-control" id="name"
+                  v-model="review.reviewerName"
+                  placeholder="Enter your name">
+         </div>
+
+         <div class="form-group">
+           <label for="review">Review:</label>
+           <textarea class="form-control" id="review" rows="5"
+                     v-model="review.message"
+                     placeholder="Write your message here..."></textarea>
+         </div>
+
+         <div class="form-group">
+           <label for="rating">Rating</label>
+           <select class="form-control" id="rating" v-model.number="review.rating">
+             <option>1</option>
+             <option>2</option>
+             <option>3</option>
+             <option>4</option>
+             <option>5</option>
+           </select>
+         </div>
+
+         <button type="submit" @click="onSubmit()" class="btn btn-info">Submit</button>
+       </form>
+     </div>
    </div>
 </template>
 
 <script>
-  import StudentService from '../shared/service/StudentService'
+  // import StudentService from '../shared/service/StudentService'
+  import axios from 'axios'
 
 export default {
   name: 'ListStudent',
@@ -53,7 +94,14 @@ export default {
       ],
       totalItems: 0,
       itemsPerPage: 10,
-      currentPage: 1
+      currentPage: 1,
+      show: false,
+      review: {
+        reviewerEmail: null,
+        reviewerName: null,
+        rating: null,
+        message: null
+      }
     }
   },
   created () {
@@ -61,12 +109,29 @@ export default {
   },
   methods: {
     getStudents () {
-      StudentService.getStudentList(this.currentPage, this.itemsPerPage)
-        .then(response => {
-          console.log(response)
-          this.students = response
-          // this.totalItems = response.totalItems
+      axios.get('http://localhost:8080/api/students/', {
+        title: this.postTitle,
+        body: this.postBody
+      }).then((response) => {
+        this.students = response.data
+      })
+        .catch((e) => {
+          console.error(e)
         })
+      // StudentService.getStudentList(this.currentPage, this.itemsPerPage)
+      //   .then(response => {
+      //     console.log(response)
+      //     this.students = response
+      //     // this.totalItems = response.totalItems
+      //   })
+    },
+    onSubmit () {
+      console.log('Submit review')
+      this.$router.reload()
+    },
+    chooseStudent (id) {
+      console.log('click view ' + id)
+      this.show = true
     }
   }
 }
